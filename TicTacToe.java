21608562c6bef3b2399c66704cc2a843ab77833b
darthkaran <karandams@gmail.com>
2018-01-16 21:01:46 -0500

    Added first source file

---
+++ TicTacToe.java
@@ -0,0 +1,31 @@
+//Karan Damodar
+//September 16/01/2018
+//TicTacToe
+
+/**************************
+Design of the Board
+Will use a 2D String array to store values
+ 1|2|3
+ 4|5|6
+ 7|8|9
+***************************/
+
+
+public class TicTacToe{
+
+  public static void main(String[] args) {
+   String[][] board = new String[3][5];
+   
+   System.out.println("");
+
+ }
+
+/*public static void printSong(Song song) {
+  int min = song.getDuration() / 60;
+  int sec = song.getDuration() % 60;
+  System.out.printf("%s (%s) - %d:%d\n",
+  song.getTitle(), song.getArtist(), min, sec);
+  }
+
+  */
+}
