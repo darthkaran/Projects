b80cf1fc22305b19ea8e757dbd539a5b12a1ffa0
darthkaran <karandams@gmail.com>
2018-01-17 13:18:05 -0500

    added a very simple computer input

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,18 +5,31 @@
 /**************************
 Design of the Board
 Will use a 2D String array to store values
- 1|2|3
- 4|5|6
- 7|8|9
-***************************/
+|0|1|2|
+|3|4|5|
+|6|7|8|
 
+board[0][0] = "0";
+board[0][1] = "1";
+board[0][2] = "2";
+
+board[1][0] = "3";
+board[1][1] = "4";
+board[1][2] = "5";
+
+board[2][0] = "6";
+board[2][1] = "7";
+board[2][2] = "8"
+
+***************************/
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class TicTacToe{
 
   public static void main(String[] args) {
-   String[][] board = new String[3][5]; //UI for the user
+   String[][] board = new String[3][3]; //UI for the user
 
    /*while(gameCondition == true){
 
@@ -27,9 +40,6 @@ public class TicTacToe{
 
    System.out.println("");
 
-
-
-
  }
 
  public static boolean gameCondition(){
@@ -38,31 +48,26 @@ public class TicTacToe{
 
  public static void printBoard(String board[][]){
    for(int i = 0; i < board.length; i++){
+     System.out.print("|");
      for(int j = 0; j < board[i].length; j++){
-       System.out.print(board[i][j]);
+       System.out.print(board[i][j] + "|");
      }
      System.out.println("");
    }
 }
 
  public static void buildBoard(String board[][]){
-   board[0][0] = "1";
-   board[0][1] = "|";
+   board[0][0] = "0";
+   board[0][1] = "1";
    board[0][2] = "2";
-   board[0][3] = "|";
-   board[0][4] = "3";
 
-   board[1][0] = "4";
-   board[1][1] = "|";
+   board[1][0] = "3";
+   board[1][1] = "4";
    board[1][2] = "5";
-   board[1][3] = "|";
-   board[1][4] = "6";
 
-   board[2][0] = "7";
-   board[2][1] = "|";
+   board[2][0] = "6";
+   board[2][1] = "7";
    board[2][2] = "8";
-   board[2][3] = "|";
-   board[2][4] = "9";
  }
 
  public static int getInput(){
@@ -72,8 +77,20 @@ public class TicTacToe{
    System.out.println("Where do you want to place your \"X\"");
    int i = scan.nextInt();
    return i;
-   //test
-   //should work now
-
  }
+
+ public static void computerInput(String board[][]){
+   Random rand = new Random();
+   int n = rand.nextInt(8) + 0;
+
+   if(n == 3 || n == 4 || n == 5){
+     board[1][n] = "O";
+   }else if(n == 6|| n == 7|| n == 8){
+     board[1][n] = "O";
+   } else{
+     board[0][n] = "O";
+   }
+ }
+
+
 }
