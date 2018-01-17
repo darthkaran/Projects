e2647eef8da51b9b96fe73aa50c92c503564b323
darthkaran <darthkaran@gmail.com>
2018-01-16 21:55:19 -0500

    added function to build a board and print the board

--- TicTacToe.java
+++ TicTacToe.java
@@ -14,10 +14,50 @@ Will use a 2D String array to store values
 public class TicTacToe{
 
   public static void main(String[] args) {
-   String[][] board = new String[3][5];
-   
+   String[][] board = new String[3][5]; //UI for the user
+
+   /*while(gameCondition == true){
+
+   }*/
+
+   buildBoard(board);
+   printBoard(board);
+
    System.out.println("");
 
+ }
+
+ public static boolean gameCondition(){
+ return true;
+ }
+
+ public static void printBoard(String board[][]){
+   for(int i = 0; i < board.length; i++){
+     for(int j = 0; j < board[i].length; j++){
+       System.out.print(board[i][j]);
+     }
+     System.out.println("");
+   }
+}
+
+ public static void buildBoard(String board[][]){
+   board[0][0] = "-";
+   board[0][1] = "|";
+   board[0][2] = "-";
+   board[0][3] = "|";
+   board[0][4] = "-";
+
+   board[1][0] = "-";
+   board[1][1] = "|";
+   board[1][2] = "-";
+   board[1][3] = "|";
+   board[1][4] = "-";
+
+   board[2][0] = "-";
+   board[2][1] = "|";
+   board[2][2] = "-";
+   board[2][3] = "|";
+   board[2][4] = "-";
  }
 
 /*public static void printSong(Song song) {
