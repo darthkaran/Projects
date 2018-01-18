3ec13e40e58de910d022b7dee10a173d02bb4e5a
darthkaran <karandams@gmail.com>
2018-01-18 17:27:07 -0500

    Added win condition

--- TicTacToe.java
+++ TicTacToe.java
@@ -31,14 +31,20 @@ public class TicTacToe{
   public static void main(String[] args) {
    String[][] board = new String[3][3]; //UI for the user
 
-   /*while(gameCondition == true){
-
-   }*/
-
    buildBoard(board);
    printBoard(board);
 
    System.out.println("");
+
+   while(!userWinCondition(board)){
+     getInput(board);
+     printBoard(board);
+     userWinCondition(board);
+     computerInput(board);
+     printBoard(board);
+     computerWinCondition(board);
+
+   }
 
  }
 
@@ -70,13 +76,19 @@ public class TicTacToe{
    board[2][2] = "8";
  }
 
- public static int getInput(){
+ public static void getInput(String board[][]){
    Scanner scan = new Scanner(System.in);
 
 
    System.out.println("Where do you want to place your \"X\"");
-   int i = scan.nextInt();
-   return i;
+   int n = scan.nextInt();
+   if(n == 3 || n == 4 || n == 5){
+     board[1][n-3] = "X";
+   }else if(n == 6|| n == 7|| n == 8){
+     board[1][n-6] = "X";
+   } else{
+     board[0][n] = "X";
+   }
  }
 
  public static void computerInput(String board[][]){
@@ -84,49 +96,61 @@ public class TicTacToe{
    int n = rand.nextInt(8) + 0;
 
    if(n == 3 || n == 4 || n == 5){
-     board[1][n] = "O";
+     board[1][n - 3] = "O";
    }else if(n == 6|| n == 7|| n == 8){
-     board[1][n] = "O";
+     board[1][n - 6] = "O";
    } else{
      board[0][n] = "O";
    }
  }
 
- public static boolean userWinCondition(char board[][]){
-   /*board[0][0] = "0";
-   board[0][1] = "1";
-   board[0][2] = "2";
-
-   board[1][0] = "3";
-   board[1][1] = "4";
-   board[1][2] = "5";
-
-   board[2][0] = "6";
-   board[2][1] = "7";
-   board[2][2] = "8";
-
-   |0|1|2|
-   |3|4|5|
-   |6|7|8|
-   */
-   if((board[0][0] == 'X') && (board[0][1] == 'X') && (board[0][2] = 'X')){
+ public static boolean userWinCondition(String board[][]){
+   if( board[0][0].equals("X") && (board[0][1].equals("X")) && (board[0][2].equals("X") ) ){
      return true;
-   } else if((board[1][0] == 'X') && (board[1][1] == 'X') && (board[1][2] = 'X')){
+   } else if((board[1][0].equals("X")) && (board[1][1].equals("X")) && (board[1][2].equals("X"))){
      return true;
-   } else if(board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] = 'X'){
+   } else if((board[2][0].equals("X")) && (board[2][1].equals("X")) && (board[2][2].equals("X"))){
      return true;
-   } else if(board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] = 'X'){
+   } else if(board[0][0].equals("X") && board[1][0].equals("X") && board[2][0].equals("X")){
      return true;
-   } else if(board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] = 'X'){
+   } else if(board[0][1].equals("X") && board[1][1].equals("X") && board[2][1].equals("X")){
      return true;
-   } else if(board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] = 'X'){
+   } else if(board[0][2].equals("X") && board[1][2].equals("X") && board[2][2].equals("X")){
      return true;
-   } else if(board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] = 'X'){
+   } else if(board[0][0].equals("X") && board[1][0].equals("X") && board[2][0].equals("X")){
      return true;
-   }else if(board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] = 'X'){
+   }else if(board[0][0].equals("X") && board[1][1].equals("X") && board[2][2].equals("X")){
       return true;
+   }else if(board[0][2].equals("X") && board[1][1].equals("X") && board[2][0].equals("X")){
+      return true;
+   }else{
+     return false;
    }
+  }
+
+
+  public static boolean computerWinCondition(String board[][]){
+    if( board[0][0].equals("O") && (board[0][1].equals("O")) && (board[0][2].equals("O") ) ){
+      return true;
+    } else if((board[1][0].equals("O")) && (board[1][1].equals("O")) && (board[1][2].equals("O"))){
+      return true;
+    } else if((board[2][0].equals("O")) && (board[2][1].equals("O")) && (board[2][2].equals("O"))){
+      return true;
+    } else if(board[0][0].equals("O") && board[1][0].equals("O") && board[2][0].equals("O")){
+      return true;
+    } else if(board[0][1].equals("O") && board[1][1].equals("O") && board[2][1].equals("O")){
+      return true;
+    } else if(board[0][2].equals("O") && board[1][2].equals("O") && board[2][2].equals("O")){
+      return true;
+    } else if(board[0][0].equals("O") && board[1][0].equals("O") && board[2][0].equals("O")){
+      return true;
+    }else if(board[0][0].equals("O") && board[1][1].equals("O") && board[2][2].equals("O")){
+       return true;
+    }else if(board[0][2].equals("O") && board[1][1].equals("O") && board[2][0].equals("O")){
+       return true;
+    }else{
+      return false;
+    }
+   }
+
  }
-
-
-}
